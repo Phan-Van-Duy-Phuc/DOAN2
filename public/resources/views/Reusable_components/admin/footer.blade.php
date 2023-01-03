ca629bbc1f9d47ac2881dabd45d0dcbc309cb5eb
Phan-Van-Duy-Phuc <73930999+Phan-Van-Duy-Phuc@users.noreply.github.com>
2023-01-03 09:56:21 +0700

    Add files via upload

---
+++ public/resources/views/Reusable_components/admin/footer.blade.php
@@ -0,0 +1,26 @@
+ 
+<!-- Footer -->
+<footer class="page-footer font-small stylish-color-white pt-4" style="width:100%;color:black;position:fixed;">
+
+ 
+   
+
+<!-- Copyright -->
+<div  class="footer-copyright text-center py-3" style=background:black>
+      <div class="row" style="font-size:15px;">
+          <div class="col-md-3">
+                  © 2022 Copyright:
+              <a href=""> Team 4</a>
+          </div>
+          
+          <div  id="developedbybtaoteam" class="col-md-9" >
+               <span class="px-5"> Developed by Team 4</span>
+          </div>
+      </div>
+
+  
+</div>
+<!-- Copyright -->
+
+</footer>
+<!-- Footer -->
